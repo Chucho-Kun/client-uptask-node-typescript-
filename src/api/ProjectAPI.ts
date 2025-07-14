4ab1c6edab3b561faf8ec006055ccdac4e0af732
Jesus Peralta <jesus_web_master@hotmail.com>
2025-07-14 13:21:21 -0600

    editing proyects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,6 +1,6 @@
 
 import api from "@/lib/axios";
-import { dashboardProjectSchema, type ProjectFormData } from "../types";
+import { dashboardProjectSchema, type Project, type ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject( formData : ProjectFormData ) {
@@ -22,6 +22,17 @@ export async function getProjects() {
         const { data } = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
         if(response.success) return response.data        
+    } catch (error) {
+        if( isAxiosError(error) && error.response){
+            throw new Error( error.response.data.error )
+        }
+    }
+}
+
+export async function getProjectsById( id : Project['_id'] ) {
+    try {
+        const { data } = await api(`/projects/${ id }`)
+            return data            
     } catch (error) {
         if( isAxiosError(error) && error.response){
             throw new Error( error.response.data.error )
