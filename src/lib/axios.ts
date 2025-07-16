bd7fa2acc504d495c926c9709d70fdcd4ba9fb2f
Jesus Peralta <jesus_web_master@hotmail.com>
2025-07-16 16:28:32 -0600

    interceptor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,13 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
+// Interceptor para mostrar la URL completa de cada request
+api.interceptors.request.use(config => {
+  const fullUrl = config.baseURL
+    ? config.baseURL.replace(/\/$/, '') + (config.url?.startsWith('/') ? config.url : '/' + config.url)
+    : config.url;
+  console.log('Petición Axios URL:', fullUrl);
+  return config;
+});
+
 export default api
